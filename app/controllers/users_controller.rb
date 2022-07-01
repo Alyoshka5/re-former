5810f7526fc24c54276e4c1d10fe67b9741cb009
Alyoshka <alyoshkamyshko@gmail.com>
2022-06-30 17:37:10 -0700

    Add editing to user accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,23 @@ class UsersController < ApplicationController
         # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
         if @user.save
-            redirect_to new_user_path
+            redirect_to edit_user_path(@user)
         else
             render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params())
+            redirect_to new_user_path
+        else
+            render :edit, status: :unprocessable_entity
         end
     end
 
